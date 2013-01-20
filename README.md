e000e96a12e12e855d03b5acfa31a9caa30cd1c1
Andreas Hultgren <andreas.hultgren@frojd.se>
2013-01-20 21:48:42 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# presspress
+
+Just trying to make a blog plattform in node real fast.
+
+## Getting Started
+_(Coming soon)_
+
+## Documentation
+_(Coming soon)_
+
+## Examples
+_(Coming soon)_
+
+## Contributing
+In lieu of a formal styleguide, take care to maintain the existing coding style. Add unit tests for any new or changed functionality. Lint and test your code using [grunt](https://github.com/cowboy/grunt).
+
+## Release History
+_(Nothing yet)_
+
+## License
+Copyright (c) 2013 Andreas Hultgren  
+Licensed under the MIT license.
---
+++ grunt.js
@@ -0,0 +1,86 @@
+"use strict";
+var pkg = require('./package.json');
+
+module.exports = function(grunt) {
+  // Project configuration.
+  grunt.initConfig({
+    pkg: '<json:package.json>',
+    test: {
+      files: ['test/**/*.js']
+    },
+    lint: {
+      files: ['grunt.js', 'lib/js/*.js', 'test/**/*.js']
+    },
+    less: {
+      development: {
+        files: {
+          "lib/public/dist/presspress.css": "lib/public/less/presspress.less"
+        }
+      },
+      production: {
+        options: {
+          yuicompress: true
+        },
+        files: {
+          "lib/public/dist/presspress.css": "lib/public/less/presspress.less"
+        }
+      }
+    },
+    concat: {
+      js: module.exports.js,
+      css: module.exports.css
+    },
+    min: {
+      js: module.exports.minjs
+    },
+    watch: {
+      files: '<config:lint.files>',
+      tasks: 'default'
+    },
+    jshint: {
+      options: {
+        curly: true,
+        eqeqeq: true,
+        immed: true,
+        latedef: true,
+        newcap: true,
+        noarg: true,
+        sub: true,
+        undef: true,
+        boss: true,
+        eqnull: true,
+        node: true
+      },
+      globals: {
+        exports: true
+      }
+    }
+  });
+
+  // npm tasks
+  grunt.loadNpmTasks('grunt-contrib-less');
+
+  // Default task.
+  grunt.registerTask('default', 'lint test less:development concat min');
+};
+
+module.exports.js = {
+  src: [
+    'lib/public/js/bootstrap.js'
+  ],
+  dest: 'lib/public/dist/' + pkg.name + '.js'
+};
+
+module.exports.css = {
+  src: [
+    'lib/public/css/bootstrap.css',
+    'lib/public/css/bootstrap-responsive.css',
+    'lib/public/dist/presspress.css'
+  ],
+  dest: 'lib/public/dist/' + pkg.name + '.min.css'
+};
+
+module.exports.minjs = {
+  src: [ '<config:concat.js.dest>' ],
+  dest: 'lib/public/dist/<%= pkg.name %>.min.js'
+};

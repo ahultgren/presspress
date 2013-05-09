7ab4d3c6197abeb15d8faee503c3197bf0520529
Andreas Hultgren <andreas.hultgren@frojd.se>
2013-05-09 21:14:06 +0200

    Moved admin theming to its own theme. It's not really optimal to manage routes there, but it'll work for now

---
+++ Gruntfile.js
@@ -0,0 +1,41 @@
+"use strict";
+
+module.exports = function(grunt) {
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    interval: 200,
+    jshint: {
+      options: {
+        curly: true,
+        eqeqeq: true,
+        immed: true,
+        latedef: false,
+        newcap: true,
+        noarg: true,
+        sub: true,
+        undef: true,
+        boss: true,
+        eqnull: true,
+        node: true,
+        es5: true,
+        strict: true,
+        globalstrict: true
+      },
+      files: {
+        src: ['Gruntfile.js', 'package.json', 'lib/**/**/*.js', '!lib/themes/**']
+      }
+    },
+    watch: {
+      files: ['<%= jshint.files.src %>'],
+      tasks: ['default']
+    }
+  });
+
+  // npm tasks
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-jshint');
+
+  // Default task.
+  grunt.registerTask('default', ['jshint']);
+};

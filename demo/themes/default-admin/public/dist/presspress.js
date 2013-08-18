a8b233a03b8d863d52d5076d3d6f48fe1e1d7f9a
Andreas Hultgren <andreas.hultgren@frojd.se>
2013-08-19 00:27:48 +0200

    Fixed old shitcode in user model and implemented bcrypt

--- demo/themes/default-admin/public/dist/presspress.js
+++ demo/themes/default-admin/public/dist/presspress.js
@@ -1,4 +1,4 @@
-/*! presspress-admin - v0.0.1 - 2013-08-18 */
+/*! presspress-admin - v0.0.1 - 2013-08-19 */
 /* ===================================================
  * bootstrap-transition.js v2.2.2
  * http://twitter.github.com/bootstrap/javascript.html#transitions
